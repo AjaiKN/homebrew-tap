753bae5a067084b59095556a68fbabb9df649736
Ajai Khatri Nelson <ajai@ajai.dev>
2025-12-02 01:15:40 -0500

    Fix brew audit

--- Formula/akn-trash.rb
+++ Formula/akn-trash.rb
@@ -1,9 +1,9 @@
 class AknTrash < Formula
-  desc "A bash CLI for putting files in the trash"
+  desc "Bash CLI for putting files in the trash"
   homepage "https://github.com/AjaiKN/trash"
-  url "https://github.com/AjaiKN/trash/archive/refs/tags/v0.0.3.zip"
-  sha256 "93e423e2c0fa4c9771dd6af2011980d21b5d0b2a4593f2c44b59a5d511c641d2"
-  license "GPL-3.0"
+  url "https://github.com/AjaiKN/trash/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "7e5c0a5819bd63fb85f8a680deb2a3d62c04d73ff0fddfd1abeb2e732945dc4c"
+  license "GPL-3.0-or-later"
 
   def install
     bin.install Dir["bin/*"]
@@ -14,10 +14,10 @@ class AknTrash < Formula
   test do
     # brew test --verbose --keep-tmp akn-trash
     shell_output "touch #{testpath}/a_file.txt"
-    assert_predicate testpath/"a_file.txt", :exist?
+    assert_path_exists testpath/"a_file.txt"
     puts shell_output "#{bin}/trash --dry-run -v -s freedesktop #{testpath}/a_file.txt 2>&1"
-    assert_predicate testpath/"a_file.txt", :exist?
+    assert_path_exists testpath/"a_file.txt"
     puts shell_output "#{bin}/trash -v -s freedesktop #{testpath}/a_file.txt 2>&1"
-    refute_predicate testpath/"a_file.txt", :exist?
+    refute_path_exists testpath/"a_file.txt"
   end
 end
